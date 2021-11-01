8c280b6b994734b13656f40359712d2f94b18ac2
Ruslan559395 <87967132+Ruslan559395@users.noreply.github.com>
2021-11-02 00:40:52 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -445,3 +445,10 @@ da.init();
 }());
 
 
+if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+  window.document.addEventListener('touchmove', e => {
+    if(e.scale !== 1) {
+      e.preventDefault();
+    }
+  }, {passive: false});
+}
