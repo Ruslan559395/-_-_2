eed09a1cc7938bb33f8c219dbd1f72e7866ba4cd
Ruslan559395 <87967132+Ruslan559395@users.noreply.github.com>
2021-11-02 00:37:33 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -446,9 +446,6 @@ da.init();
 
 
 
-document.addEventListener('touchmove', function(event) {
-    event = event.originalEvent || event;
-    if(event.scale > 1) {
-      event.preventDefault();
-    }
-  }, false);
+document.addEventListener('gesturestart', function (e) {
+    e.preventDefault();
+});
